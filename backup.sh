99c4a12ce5b8c117885555c40b045ecc78499291
AllHailTheSheep <root@alpine.julia.me>
2024-01-15 15:34:58 -0500

    update backup.sh to use rsync

--- backup.sh
+++ backup.sh
@@ -2,13 +2,17 @@
 # TODO: make this so we dont copy files that already exist and havent been updated (maybe have same number of bytes on backup and primary drive?)
 
 echo "Copying scripts..."
-cp -rf /home/julia/scripts/** /home/julia/hdd3/backup/scripts
+# cp -uvrf /home/julia/scripts/** /home/julia/hdd3/backup/scripts
+rsync -vvhru --progress --delete /home/julia/scripts/ /home/julia/hdd3/backup/scripts
 
 echo "Copying hdd0..."
-cp -rf /home/julia/hdd0/** /home/julia/hdd3/backup/hdd0
+# cp -uvrf /home/julia/hdd0/** /home/julia/hdd3/backup/hdd0
+rsync -vvhru --progress --delete /home/julia/hdd0/ /home/julia/hdd3/backup/hdd0
 
 echo "Copying hdd1..."
-cp -rf /home/julia/hdd1/** /home/julia/hdd3/backup/hdd1
+# cp -uvrf /home/julia/hdd1/** /home/julia/hdd3/backup/hdd1
+rsync -vvhru --progress --delete /home/julia/hdd1/ /home/julia/hdd3/backup/hdd1
 
 echo "Copying hdd2..."
-cp -rf /home/julia/hdd2/** /home/julia/hdd3/backup/hdd2
+# cp -uvrf /home/julia/hdd2/** /home/julia/hdd3/backup/hdd2
+rsync -vvhru --progress --delete /home/julia/hdd2/ /home/julia/hdd3/backup/hdd2
